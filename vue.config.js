585aae9f6ff6d6a8cfb074ab1b59c1ffdfabb76c
Allen Penate <allenson@gmail.com>
2021-08-11 11:53:35 -0400

    Add Hypernova service

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 module.exports = {
+  configureWebpack:{
+    target: 'web',
+    node: {
+      fs: 'empty',
+      module: 'empty'
+    },      
+    entry: {
+      client: [
+        './src/nova.js'
+      ]
+    }
+  },
   transpileDependencies: [
     'vuetify'
   ]
